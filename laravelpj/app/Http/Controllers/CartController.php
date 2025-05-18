96e9a5e601fc1c10f8468e3a99a7636719c894d3
minhtri123442 <minhtrichautran@gmail.com>
2025-05-18 11:07:20 +0700

    ShoppingCart

--- laravelpj/app/Http/Controllers/CartController.php
+++ laravelpj/app/Http/Controllers/CartController.php
@@ -1,6 +1,5 @@
 <?php
 
-// app/Http/Controllers/CartController.php
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,22 +8,21 @@ use App\Models\CrudProduct;
 class CartController extends Controller
 {
     public function addToCart($id)
-{
-    $product = CrudProduct::findOrFail($id);
-    $cart = session()->get('cart', []);
+    {
+        $product = CrudProduct::findOrFail($id);
+        $cart = session()->get('cart', []);
 
-    $cart[$id] = [
-        'id' => $product->id,
-        'name' => $product->name,    
-        'price' => $product->price,
-        'image' => $product->image,
-        'quantity' => isset($cart[$id]) ? $cart[$id]['quantity'] + 1 : 1,
-    ];
+        $cart[$id] = [
+            'id' => $product->id,
+            'name' => $product->name,
+            'price' => $product->price,
+            'image' => $product->image,
+            'quantity' => isset($cart[$id]) ? $cart[$id]['quantity'] + 1 : 1,
+        ];
 
-    session()->put('cart', $cart);
-    return redirect()->back()->with('success', 'Đã thêm vào giỏ hàng thành công!');
-}
-
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Đã thêm vào giỏ hàng thành công!');
+    }
 
     public function viewCart()
     {
@@ -39,4 +37,38 @@ class CartController extends Controller
         session()->put('cart', $cart);
         return redirect()->route('cart.view')->with('success', 'Đã xóa sản phẩm khỏi giỏ hàng');
     }
+
+    public function updateCart(Request $request)
+    {
+        $cart = session()->get('cart', []);
+
+        // Tăng số lượng
+        if ($request->has('increase')) {
+            $id = $request->input('increase');
+            if (isset($cart[$id])) {
+                $cart[$id]['quantity']++;
+            }
+        }
+
+        // Giảm số lượng
+        if ($request->has('decrease')) {
+            $id = $request->input('decrease');
+            if (isset($cart[$id]) && $cart[$id]['quantity'] > 1) {
+                $cart[$id]['quantity']--;
+            }
+        }
+
+        // Thanh toán (checkout)
+        if ($request->has('checkout')) {
+            // Có thể xử lý lưu đơn hàng vào DB ở đây nếu muốn
+            session()->forget('cart');
+            return redirect()->route('cart.view')->with('success', 'Thanh toán thành công!');
+        }
+        
+
+        session()->put('cart', $cart);
+        return redirect()->route('cart.view');
+        
+    }
+    
 }
